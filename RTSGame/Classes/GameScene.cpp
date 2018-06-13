d7fde92416f2d805316512bee7f119c763889551
sushaoci <sushaoci@sina.com>
2018-06-14 00:20:12 +0800

    minimap improvement

--- RTSGame/Classes/GameScene.cpp
+++ RTSGame/Classes/GameScene.cpp
@@ -26,6 +26,7 @@ static TMXTiledMap *PlayMap;
 time_t initTime;
 time_t Time;
 Label* gametime;
+Sprite* em;
 
 
 Scene* GameScene::createScene()
@@ -96,6 +97,10 @@ bool GameScene::init()
 	auto minimap = Sprite::create("tem/minimap.png");
 	minimap->setPosition(Vec2(150,150));
 	this->addChild(minimap, 300);
+	em = Sprite::create("tem/em.png");
+	em->setPosition(Vec2(em->getContentSize().width / 2,em->getContentSize().height / 2));
+	em->setAnchorPoint(Vec2(0.5, 0.5));
+	this->addChild(em, 300);
 	//map->setScale(1 / 0.78125, 1 / 0.78125);
 
 	//log("%f %f", map->getContentSize().width, map->getContentSize().height);
@@ -171,7 +176,13 @@ bool GameScene::init()
 
 				}
 			}
-
+			//实验功能
+			if (ContainRect(position, Vec2(0, 300), Vec2(300, 0))) {
+				if (!(PlayMap->numberOfRunningActions())) {				//这一句是为了地图视角移动更加流畅
+					auto map_move = MoveTo::create(1.0f, Vec2(-position.x * 40 / 3+800,-position.y*40/3+450));
+					PlayMap->runAction(map_move);
+				}
+			}
 			else {
 				if (building) {															//建造状态
 					if (!virtual_factory.empty()) {										//虚建筑容器非空，即跟随鼠标移动的透明建筑存在
@@ -332,6 +343,8 @@ bool GameScene::init()
 			}
 
 		}
+			auto t = target - position;
+			em->setPosition(Vec2(t.x * 3 / 40 + em->getContentSize().width / 2, t.y * 3 / 40 + em->getContentSize().height / 2));
 	};
 	auto dispatcher = Director::getInstance()->getEventDispatcher();					//添加鼠标事件监听器
 	dispatcher->addEventListenerWithSceneGraphPriority(MouseReply, this);
@@ -740,6 +753,7 @@ void GameScene::updateTime(float di)
 }
 void GameScene::updateMini(float di)
 {
+	//soldier
 	for (int i = 0; i < 4; ++i)
 	{
 		int j = 0;
@@ -758,4 +772,5 @@ void GameScene::updateMini(float di)
 			j+= 1;
 		}
 	}
+	//building
 }
